4ae9cb6cf7a4de3fc744f0ef7f0aa1e351406ac8
LocalPiper <114950205+LocalPiper@users.noreply.github.com>
2023-03-04 16:27:05 +0300

    beta 0.12.5

--- src/main/java/app/App.java
+++ src/main/java/app/App.java
@@ -1,32 +1,34 @@
 package app;
 
 import com.opencsv.exceptions.CsvException;
+import commands.Client;
+import commands.Receiver;
 import converters.CSVtoStringListConverter;
 import converters.StringListToObjectVectorConverter;
 import datatype.Vehicle;
-import commands.Client;
-import commands.Receiver;
 
-import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.List;
 import java.util.Vector;
 
 public class App {
 
     public static void main(String[] args) throws IOException, CsvException {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        CSVtoStringListConverter csvToString = new CSVtoStringListConverter(reader);
-        List<String[]> text = csvToString.convertCSVtoStringList();
-        if (text != null) {
-            StringListToObjectVectorConverter stringToVector = new StringListToObjectVectorConverter(text);
-            Vector<Vehicle> dataSet = stringToVector.convertStringListToObjectVector();
-            System.out.println();
+        CSVtoStringListConverter csvToString = new CSVtoStringListConverter();
+        try {
+            List<String[]> text = csvToString.convertCSVtoStringList();
+            if (text != null) {
+                StringListToObjectVectorConverter stringToVector = new StringListToObjectVectorConverter(text);
+                Vector<Vehicle> dataSet = stringToVector.convertStringListToObjectVector();
+                System.out.println();
 
-            Receiver receiver = new Receiver(dataSet, csvToString.getVarAddress(), stringToVector.getVehicleFactory());
-            Client client = new Client(receiver);
-            client.runningMode();
+                Receiver receiver = new Receiver(dataSet, csvToString.getVarAddress(), stringToVector.getVehicleFactory());
+                Client client = new Client(receiver);
+                client.runningMode();
+            }
+        } catch (FileNotFoundException fileNotFoundException) {
+            System.out.println("File " + fileNotFoundException.getMessage() + " not found");
         }
     }
 }
--- src/main/java/converters/CSVtoStringListConverter.java
+++ src/main/java/converters/CSVtoStringListConverter.java
@@ -3,91 +3,38 @@ package converters;
 import com.opencsv.CSVReader;
 import com.opencsv.exceptions.CsvException;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
+import java.io.*;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.List;
-import java.util.Objects;
+import java.util.Optional;
 
 public class CSVtoStringListConverter {
-    private final BufferedReader reader;
-    private Path varAddress;
-
-    public CSVtoStringListConverter(BufferedReader reader) {
-        this.reader = reader;
-    }
+    private Path varPath;
 
     public Path getVarAddress() {
-        return varAddress;
+        return varPath;
     }
 
     public List<String[]> convertCSVtoStringList() throws IOException, CsvException {
-        InputStreamReader readDataFromFile = getVariableNameFromUser(reader);
-        if (readDataFromFile == null) {
-            return null;
+        String fileName = "FILE";
+        InputStreamReader reader;
+        if (openFileWithPathFromEnv(fileName).isPresent()) {
+            reader = new InputStreamReader(new FileInputStream(openFileWithPathFromEnv(fileName).get()));
+            CSVReader csvReader = new CSVReader(reader);
+            return csvReader.readAll();
         }
-        CSVReader csvReader = new CSVReader(readDataFromFile);
-        return csvReader.readAll();
+        throw new FileNotFoundException(fileName);
     }
 
-    public InputStreamReader getVariableNameFromUser(BufferedReader reader) throws IOException {
-        boolean collectionCreationForbidden = false;
-        InputStreamReader readDataFromFile = null;
-        while (!collectionCreationForbidden) {
-            System.out.println("Input variable name:\n(NOTE: Type \"exit\" to leave the program).");
-            String userInput = reader.readLine();
-            if (userInput == null || checkIfUserLeaves(userInput)) {
-                System.out.println("Leaving the program");
-                break;
-            }
-            try {
-                varAddress = Paths.get(System.getenv(userInput));
-                readDataFromFile = new InputStreamReader(Files.newInputStream(Paths.get(String.valueOf(varAddress))), StandardCharsets.UTF_8);
-                collectionCreationForbidden = true;
-            } catch (NullPointerException npe) {
-                readDataFromFile = handleNullName(reader);
-                collectionCreationForbidden = true;
-            }
+    public Optional<File> openFileWithPathFromEnv(String fileName) {
+        String path = System.getenv(fileName);
+        if (path != null) {
+            varPath = Path.of(path);
+            return Optional.of(new File(path));
+        } else {
+            return Optional.empty();
         }
-        return (collectionCreationForbidden) ? readDataFromFile : null;
-    }
 
-    public InputStreamReader handleNullName(BufferedReader reader) throws IOException {
-        while (true) {
-            System.out.println("""
-                    Environment variable not found. Choose from 2 options:
-                    1. Try again.
-                    2. Create empty file.
-                    Print either 1 or 2 into the console.""");
-            String userRequest = reader.readLine();
-            if (userRequest == null || checkIfUserLeaves(userRequest.strip())) {
-                System.out.println("Leaving the program");
-                return null;
-            }
-            if (Objects.equals(userRequest.strip(), "1")) {
-                return getVariableNameFromUser(reader);
-            } else if (Objects.equals(userRequest.strip(), "2")) {
-                File empty_file = new File("C:\\Users\\Honor\\IdeaProjects\\App\\src\\main\\java\\files\\empty_file.csv");
-                varAddress = empty_file.toPath();
-                if (empty_file.createNewFile()) {
-                    System.out.println("Empty file created successfully.");
-                } else {
-                    System.out.println("Empty file already exists");
-                }
-                return new InputStreamReader(Files.newInputStream(empty_file.toPath()), StandardCharsets.UTF_8);
-            } else {
-                System.out.println("Wrong input!");
-            }
-        }
-    }
-
-    public boolean checkIfUserLeaves(String userInput) {
-        return (Objects.equals(userInput.strip().toLowerCase(), "exit"));
     }
 }
 
